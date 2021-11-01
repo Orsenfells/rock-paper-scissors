d688c7229f63380e10f23196c24263fbd58817fc
Leo <leonelsoto17@gmail.com>
2021-11-01 08:47:57 -0500

    playRound function

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 function getRandomMove() {
-    let availableMoves = ['Paper', 'Rock', "scissors"];
+    let availableMoves = ['paper', 'rock', "scissors"];
     
     return availableMoves[Math.floor(Math.random()*availableMoves.length)]
 }
 
 function computerPlay() {
     return getRandomMove()
+}
+
+function playRound(playerSelection, computerPlay) {
+    if(playerSelection.toLowerCase() === 'rock') {
+        if(computerPlay === 'scissors') {
+            return "You win! Rock beats scissors"
+        } else if (computerPlay === 'paper') {
+            return "You Lose! Paper beats Rock"
+        } else if (computerPlay === 'rock') {
+            return "It's a tie! You both played Rock"
+        }
+    }
+    else if (playerSelection.toLowerCase() === 'paper') {
+        if(computerPlay === 'scissors') {
+            return 'You Lose! Scissors beats Paper'
+        } else if (computerPlay === 'paper') {
+            return "It's a tie! You both played paper"
+        } else if (computerPlay === 'rock') {
+            return "You win! Paper beats rock"
+        }
+    }
+    else if (playerSelection.toLowerCase() === 'scissors') {
+        if(computerPlay === 'scissors') {
+            return "It's a tie! You both played scissors"
+        } else if (computerPlay === 'paper') {
+            return "You win! Scissors beats paper"
+        } else if (computerPlay === 'rock') {
+            return "You Lose! Rock beats scissors"
+        }
+    }
 }
